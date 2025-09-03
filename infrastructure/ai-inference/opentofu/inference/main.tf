9d34443838bc9ee0ebb3120950325c1ed9de73a0
Luke Brady <lbrad23105@gmail.com>
2025-09-03 00:04:33 -0400

    Adding Gemma3 inference server

--- infrastructure/ai-inference/opentofu/inference/main.tf
+++ infrastructure/ai-inference/opentofu/inference/main.tf
@@ -10,10 +10,10 @@ module "qwen3_0_6b" {
   key_name             = "qwen3_0_6b_key"
 
   allowed_ip_addresses = var.allowed_ip_addresses
-  hugging_face_token = var.hugging_face_token
-  model              = "Qwen/Qwen3-0.6B"
-  vllm_timeout       = 360
-  vllm_version       = "latest"
+  hugging_face_token   = var.hugging_face_token
+  model                = "Qwen/Qwen3-0.6B"
+  vllm_timeout         = 360
+  vllm_version         = "latest"
 }
 
 module "gpt_oss_20b" {
@@ -26,8 +26,25 @@ module "gpt_oss_20b" {
   key_name             = "gpt_oss_20b_key"
 
   allowed_ip_addresses = var.allowed_ip_addresses
-  hugging_face_token = var.hugging_face_token
-  model              = "openai/gpt-oss-20b"
-  vllm_timeout       = 600
-  vllm_version       = "v0.10.1"
+  hugging_face_token   = var.hugging_face_token
+  model                = "openai/gpt-oss-20b"
+  vllm_timeout         = 600
+  vllm_version         = "v0.10.1"
+}
+
+module "gemma_3_27b" {
+  source = "../modules/inference"
+
+  ami_id               = data.aws_ami.ai_inference.id
+  instance_type        = "g6e.xlarge"
+  vpc_id               = data.aws_vpc.default.id
+  subnet_id            = data.aws_subnets.default.ids[0]
+  iam_instance_profile = data.aws_iam_instance_profile.ai_inference_profile.name
+  key_name             = "gemma_3_27b_key"
+
+  allowed_ip_addresses = var.allowed_ip_addresses
+  hugging_face_token   = var.hugging_face_token
+  model                = "google/gemma-3-27b-it"
+  vllm_timeout         = 900
+  vllm_version         = "latest"
 }
